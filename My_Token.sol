1d356b21783c654e7d1b80a557c876c6f494f96a
shreyash1710 <145668266+shreyash1710@users.noreply.github.com>
2023-09-21 16:09:33 +0530

    Add files via upload

---
+++ My_Token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract My_Token {
+    // token details
+    string public TokenName = "SHREYASH"; 
+    string public TekonID = "SH"; 
+    uint public totalSupply = 0;
+    
+    // mapping variable here
+    mapping(address => uint) public balance;
+    
+    // minting token function
+    function mint (address _address, uint _value) public {
+        totalSupply += _value; 
+        balance[_address] += _value;
+    }
+    
+    // burning token function
+    function burn (address _address, uint _value) public {
+        if (balance[_address] > _value){
+            totalSupply -= _value;
+            balance[_address] -= _value;
+        }
+    }
+}
